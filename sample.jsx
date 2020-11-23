dc7f6279ced208009c6dbfc76f4410057764703a
Behnam Aghajani <uncocoder@gmail.com>
2020-11-24 01:55:34 +0330

    Update sample.jsx

--- sample.jsx
+++ sample.jsx
@@ -2,8 +2,25 @@
   <h1>Hello Worlds</h1>
   <span class="class1 {test()} class2">{test()}</span>
   <xlink text="CLICK HERE" href="bbbb">aaaaaaaa</xlink>
+  <span class="">THIS WONT UPDATE</span>
+  <span>{count()}</span>
+  <ul>{tags()}</ul>
 </render>
 
+function tags() {
+  let items = [];
+  $.each(data.tags, function() {
+    console.log(this);
+    items.push('<li>' + this + '</li>');
+  });
+
+  return items.join(' ');
+}
+
 function test() {
-  return 'some';
+  return data.host;
+}
+
+function count() {
+  return data.tags.length;
 }
